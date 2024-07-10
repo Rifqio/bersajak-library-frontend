8bc7fd488d72917a4a1ebd7f358ae8f237850d2a
Rifqi Oktario <oktariorifqi@gmail.com>
2024-07-10 15:57:16 +0700

    feat: start quiz dialog when audio book end

--- src/pages/book/audio-book.jsx
+++ src/pages/book/audio-book.jsx
@@ -1,25 +1,27 @@
 import { fetcher, fetcherWithRange } from "@/lib/fetcher";
 import { useSwr } from "@/lib/swr";
-import PropTypes from "prop-types";
+import { StartQuizDialog } from "@/sections/quiz/start-quiz-dialog";
+import { useState } from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import { useParams } from "react-router-dom";
 
-const Player = ({ filename }) => {
-    const { data: audioData, error: audioError } = useSwr(`/audio/${filename}`, fetcherWithRange);
-    
-    if (audioError) return <div>Error loading audio</div>;
-    if (!audioData) return <div>Loading...</div>;
-    
-    const audioUrl = URL.createObjectURL(audioData);
-    
-    return <AudioPlayer autoPlay src={audioUrl} />;
-};
-
 const AudioBookPage = () => {
     const { id } = useParams();
+    const [onEndAudio, setOnEndAudio] = useState(false);
     const { data } = useSwr(`/book/${id}`, fetcher);
+
+    const { data: audioData, error: audioError } = useSwr(
+        `/audio/${id}`,
+        fetcherWithRange
+    );
+
+    if (audioError) return <div>Error loading audio</div>;
+    if (!audioData) return <div>Loading...</div>;
+
+    const audioUrl = URL.createObjectURL(audioData);
     const book = data?.data;
+
     return (
         <div className='flex flex-col items-center p-4'>
             <div className='flex justify-center mb-4'>
@@ -29,13 +31,16 @@ const AudioBookPage = () => {
                     alt='Book Cover'
                 />
             </div>
-            <Player filename={id} />
+
+            <AudioPlayer
+                onEnded={() => setOnEndAudio(true)}
+                autoPlay
+                src={audioUrl}
+            />
+
+            <StartQuizDialog open={onEndAudio} onOpenChange={setOnEndAudio} />
         </div>
     );
-};
-
-Player.propTypes = {
-    filename: PropTypes.string.isRequired
 };
 
 export default AudioBookPage;
---
+++ src/sections/quiz/start-quiz-dialog.jsx
@@ -0,0 +1,67 @@
+import {
+    Button,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle
+} from "@/components";
+import { ArrowDownAZ, ClipboardPen } from "lucide-react";
+import PropTypes from "prop-types";
+import { useNavigate, useParams } from "react-router-dom";
+
+const QuizOption = ({ icon, title, onClick }) => {
+    return (
+        <button onClick={onClick} className='border-2 border-gray-500 rounded-lg bg-gray-100 w-40 h-40 flex flex-col justify-center items-center cursor-pointer hover:bg-gray-300'>
+            {icon}
+            <h1 className='text-md font-semibold text-gray-900 font-poppins'>
+                {title}
+            </h1>
+        </button>
+    );
+};
+
+export const StartQuizDialog = ({ open, onOpenChange }) => {
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    const onClickMultipleChoice = () => {
+        navigate(`/quiz/multiple-choice/${id}`);
+        onOpenChange();
+    }
+
+
+    const onClickWordCompletion = () => {
+        navigate(`/quiz/word-completion/${id}`);
+        onOpenChange();
+    }
+
+    return (
+        <Dialog open={open} onOpenChange={onOpenChange}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Ingin memulai quiz?</DialogTitle>
+                    <DialogDescription className='pb-6'>
+                        Selamat, kamu telah menyelesaikan cerita. Silahkan pilih tipe quiz
+                    </DialogDescription>
+                    <div className='flex justify-evenly items-center pb-6 gap-10'>
+                       <QuizOption onClick={onClickMultipleChoice} icon={<ArrowDownAZ className="opacity-80" size={100} />} title='Pilihan Ganda' />
+                       <QuizOption onClick={onClickWordCompletion} icon={<ClipboardPen className="opacity-80" size={100} />} title='Lengkapi Kata' />
+                    </div>
+                       <Button onClick={() => onOpenChange()} variant="destructive">Batalkan</Button>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+QuizOption.propTypes = {
+    icon: PropTypes.node.isRequired,
+    title: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired
+};
+
+StartQuizDialog.propTypes = {
+    open: PropTypes.bool.isRequired,
+    onOpenChange: PropTypes.func.isRequired
+};
