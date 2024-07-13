7f57a7446da289fc05737128bafb97b53410eade
Ricky Firmansyah <rickyfirmansyah2707@gmail.com>
2024-07-13 12:02:06 +0700

    combine and fix multiple choice

--- src/pages/quiz/multiple-choice.jsx
+++ src/pages/quiz/multiple-choice.jsx
@@ -6,8 +6,7 @@ import { CancelDialog } from "@/sections/quiz";
 import { CircleCheck } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useMicrophone } from "@/hooks";
-import { usePost, useSwr } from "@/lib/swr";
+import useSWR from "swr";
 import { get } from "lodash";
 import { fetcher } from "@/lib/fetcher";
 import { ToastContainer, toast } from "react-toastify";
@@ -15,105 +14,92 @@ import "react-toastify/dist/ReactToastify.css";
 import ScoreDialog from "@/sections/quiz/score-dialog";
 import axios from "@/lib/axios";
 import { HOVER_COLORS, OPTION_COLORS } from "@/lib/theme";
-import { useSpeechRecognition } from "react-speech-recognition";
-
-function validateIndex(questionData) {
-  const correctAnswer = questionData?.answer || "";
-  const sanitized = questionData?.options.findIndex(
-    (option) => option.key === correctAnswer
-  );
-
-  return sanitized;
-}
-
-function validateTranscript(transcript, questionData) {
-  const sanitized = transcript?.charAt(0);
-  const index = questionData?.options.findIndex(
-    (option) => option.key.charAt(0) === sanitized
-  );
-
-  return index;
-}
+import SpeechRecognition, {
+  useSpeechRecognition
+} from "react-speech-recognition";
+import { useSwr } from "@/lib/swr";
+import { getIndexFromKey } from "@/lib/utils";
+import { useMicrophone } from "@/hooks";
 
 export const MultipleChoicePage = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  const { startListening, stopListening } =
-    useMicrophone();
 
   const [countdown, setCountdown] = useState(40);
   const [cancelQuiz, setCancelQuiz] = useState(false);
   const [isShowScore, setIsShowScore] = useState(false);
+  const [isPlayingIntro, setIsPlayingIntro] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState("");
   const [selectedOption, setSelectedOption] = useState("");
+  const [answerAudioUrl, setAnswerAudioUrl] = useState(null);
   const [numberQuiz, setNumberQuiz] = useState(1);
   const [score, setScore] = useState(0);
-  const [stepAudio, setStepAudio] = useState(1);
   const audioRef = useRef(null);
+  const introRef = useRef(null);
+  const answerAudioRef = useRef(null);
 
   // FETCH QUESTION
   const { data: questionResponse } = useSwr(
     `/quiz/multiple-choice/${id}?number=${numberQuiz}`,
-    fetcher,
-    {
-      shouldRetryOnError: false,
-      revalidateOnFocus: false
-    }
+    fetcher
   );
 
+  const { data: introAudio } = useSwr(`/guide/multiple-choice`, fetcher);
+  const { data: answerAudio } = useSWR(answerAudioUrl, fetcher);
+
   const questionList = questionResponse?.data;
+  const keyAnswer = ["a", "b", "c", "d"];
   const audioUrl = questionResponse?.data?.question_audio_url || "";
   const totalQuestion = 5;
   const optionList = get(questionList, "options", []);
-  const answer = get(questionList, "answer", "");
   const question = get(questionList, "question", "");
-  const keys = optionList.map(item => item.key);
   const commands = [
     {
-      command: keys,
-      callback: ({ command }) => {
-        if (command.includes(answer)) {
-          setScore((prevScore) => prevScore + 100 / totalQuestion);
-        }
+      command: keyAnswer,
+      callback: async ({ command }) => {
+        const value = {
+          key: command
+        };
+        const index = getIndexFromKey(optionList, command);
+        await onSelectedAnswer(index, value);
       },
       matchInterim: true
     }
   ];
 
-  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
-
-
-  // POST ANSWER
-  const validCommands = commands.map((cmd) => cmd.command).flat();
-  const isValidCommand = validCommands.some((cmd) =>
-    transcript.includes(cmd.toLowerCase())
-  );
-  const foundCommands = validCommands.filter((cmd) =>
-    transcript.includes(cmd.toLowerCase())
-  );
-  const answerData = foundCommands.length > 0 ? foundCommands[0] : "";
-  const usePostQuizAnswer = (url, body) => {
-    const { mutate: validateAnswer } = usePost(url, body);
-    return validateAnswer;
-  };
-  const validateAnswer = usePostQuizAnswer(`/quiz/multiple-choice/${id}`, {
-    number: numberQuiz,
-    answer: answerData
-  });
-
-  // HANDLE FUNCTION
-  const handleCheckAnswerd = (key, qeustionAnswer) => {
-    if(key === qeustionAnswer) {
-      setScore((prevScore) => prevScore + 100 / totalQuestion);
-    }
-  }
+  const { listening } = useSpeechRecognition({ commands });
+  const { startListening } = useMicrophone();
 
   const onSelectedAnswer = async (index, value) => {
     setSelectedIndex(index);
     setSelectedOption(value.key);
-    handleCheckAnswerd(value.key, answer)
+
+    try {
+      await axios.post(`/quiz/multiple-choice/${id}`, {
+        answer: value.key,
+        number: numberQuiz
+      });
+      setScore((prevScore) => prevScore + 100 / totalQuestion);
+      setAnswerAudioUrl("/guide/answer?type=correct");
+      toast.success("Jawaban benar!", {
+        autoClose: 2000
+      });
+    } catch (error) {
+      if (error.response.status === 400) {
+        setAnswerAudioUrl("/guide/answer?type=wrong");
+        toast.error("Jawaban salah!", {
+          autoClose: 2000
+        });
+      }
+      console.error("Error posting answer:", error);
+    }
+
+    answerAudioRef.current.play().catch((error) => {
+      console.error("Error playing the audio:", error);
+    });
   };
 
+  // HANDLE FUNCTION
   const getBackgroundColor = (index, isHovered) => {
     if (selectedIndex === index) {
       return HOVER_COLORS[index % HOVER_COLORS.length];
@@ -140,31 +126,21 @@ export const MultipleChoicePage = () => {
     setNumberQuiz((prevPage) => prevPage + 1);
   };
 
-  const handleNextQuiz = () => {
-    const roundedScore = score.toFixed(1);
-    if (numberQuiz >= totalQuestion - 1) {
-      setTimeout(() => {
-        setSelectedIndex("");
-        setSelectedOption("");
-        stopListening();
-        resetTranscript();
-        setIsShowScore(true);
-        setStepAudio(3);  
-      }, 3000);
-    } else {
-      toast.success(`skor anda adalah ${roundedScore}`, {
-        position: "top-center",
-        autoClose: 1000,
-        pauseOnHover: false,
+  useEffect(() => {
+    if (!isPlayingIntro && audioUrl && audioRef.current && numberQuiz > 1) {
+      audioRef.current.play().catch((error) => {
+        console.error("Error playing the audio:", error);
       });
-      setTimeout(() => {
-        setCountdown(40);
-        setSelectedIndex("");
-        setSelectedOption("");
-        stopListening();
-        handleNext();
-        resetTranscript();
-      }, 3000);
+    }
+  }, [audioUrl]);
+
+  const handleNextQuiz = () => {
+    if (numberQuiz >= totalQuestion) {
+      setIsShowScore(true);
+    } else {
+      handleNext();
+      setSelectedIndex("");
+      setCountdown(40);
     }
   };
 
@@ -172,12 +148,25 @@ export const MultipleChoicePage = () => {
     navigate(ROUTE.Home);
   };
 
-  // TRIGGER EFFECT
   useEffect(() => {
-    if (isValidCommand) {
-      validateAnswer();
+    setIsPlayingIntro(true);
+    introRef.current.play().catch((error) => {
+      console.error("Error playing the audio:", error);
+    });
+  }, [introAudio]);
+
+  useEffect(() => {
+    if (numberQuiz > 1 && audioRef.current) {
+      audioRef.current.play().catch((error) => {
+        console.error("Error playing the audio:", error);
+      });
     }
-  }, [isValidCommand]);
+  }, [numberQuiz, audioUrl]);
+
+  const stopListeningAndClearTimer = (timer) => {
+    clearInterval(timer);
+    SpeechRecognition.stopListening();
+  };
 
   useEffect(() => {
     let timer;
@@ -186,106 +175,15 @@ export const MultipleChoicePage = () => {
         setCountdown((prevCountdown) => prevCountdown - 1);
       }, 1000);
     }
-    if (countdown === 35) {
-      if (!isShowScore && audioUrl && audioRef.current && stepAudio === 2) {
-        audioRef.current.play();
-      }
-    }
+
      if (countdown <= 15 && countdown > 0) {
        startListening();
-     }
-     if (countdown === 0 && !isShowScore) {
-       handleNextQuiz();
      }
 
     return () => {
       clearInterval(timer);
     };
-  }, [countdown, startListening]);
-
-  useEffect(() => {
-    if (transcript.includes(answer)) {
-      setSelectedIndex(validateIndex(questionList));
-    } else {
-      setSelectedIndex(
-        validateTranscript(transcript, questionList)
-      );
-    }
-  }, [transcript]);
-
-  //AUDITO SECTION
-  const onEndedGreeting = () => {
-    setStepAudio(2);
-  };
-  const onEnndedScored = () => {
-    setStepAudio(4);
-  };
-
-  const audioScoreRef = useRef(null);
-  const audioIntroRef = useRef(null);
-  const audioOutroRef = useRef(null);
-
-  const { data: introData } = useSwr(`/guide/games?type=intro`, fetcher);
-  const audioIntroUrl = introData?.data;
-  const { data: outroData } = useSwr(`/guide/games?type=outro`, fetcher);
-  const audioOutroUrl = outroData?.data;
-  const { data: scoreData } = useSwr(`/guide/score?score=${score}`, fetcher);
-  const audioScoreUrl = scoreData?.data;
-
-  useEffect(() => {
-    const playAudio = async () => {
-      try {
-        if (audioIntroUrl && audioIntroRef.current && stepAudio === 1) {
-          await audioIntroRef.current.play();
-        }
-  
-        if (isShowScore && audioScoreUrl && audioScoreRef.current && stepAudio === 3) {
-          await audioScoreRef.current.play();
-        }
-  
-        if (isShowScore && audioOutroUrl && audioOutroRef.current && stepAudio === 4) {
-          await audioOutroRef.current.play();
-        }
-      } catch (error) {
-        console.error("Error playing audio:", error);
-      }
-    };
-  
-    playAudio();
-  }, [audioUrl, audioOutroUrl, audioIntroUrl, audioScoreUrl, isShowScore, stepAudio]);
-
-  const AudioSection = () => {
-    return (
-      <>
-        <audio
-          autoPlay={stepAudio === 1}
-          ref={audioIntroRef}
-          onEnded={onEndedGreeting}
-          src={audioIntroUrl}
-          className='hidden'
-        />
-        <audio
-          autoPlay={stepAudio === 2}
-          ref={audioRef}
-          src={audioUrl}
-          className='hidden'
-        />
-        <audio
-          autoPlay={stepAudio === 3}
-          ref={audioScoreRef}
-          onEnded={onEnndedScored}
-          src={audioScoreUrl}
-          className='hidden'
-        />
-        <audio
-          autoPlay={stepAudio === 4}
-          ref={audioOutroRef}
-          src={audioOutroUrl}
-          className='hidden'
-        />
-      </>
-    );
-  };
+  }, [listening, startListening]);
 
   // RENDER FUNCTION
   const renderCheckmark = (index) => {
@@ -300,19 +198,34 @@ export const MultipleChoicePage = () => {
     }
   };
 
+  const onStartListening = () => {
+    SpeechRecognition.startListening({ continuous: true, language: "id-ID" });
+  };
 
   return (
     <div className='mt-16 h-screen flex flex-col'>
-      {/* {audioUrl && (
-        <audio
-          ref={audioRef}
-          autoPlay
-          controls
-          src={audioUrl}
-          className='hidden'
-        />
-      )} */}
-      {AudioSection()}
+      <audio
+        ref={audioRef}
+        onEnded={() => onStartListening()}
+        onPlay={() => SpeechRecognition.stopListening()}
+        src={audioUrl}
+        className='hidden'
+      />
+      <audio
+        onEnded={() => {
+          audioRef.current.play();
+        }}
+        onPlaying={() => SpeechRecognition.stopListening()}
+        ref={introRef}
+        className='hidden'
+        src={introAudio?.data}
+      />
+      <audio
+        ref={answerAudioRef}
+        onEnded={handleNextQuiz}
+        src={answerAudio?.data}
+        className='hidden'
+      />
       <ToastContainer />
       <Progress
         value={(countdown / 40) * 100}
@@ -327,6 +240,7 @@ export const MultipleChoicePage = () => {
       <div className='grid grid-cols-4 gap-4 text-center flex-grow'>
         {optionList.map((option, index) => (
           <button
+            disabled={!introRef.current}
             key={option.key}
             onClick={() => onSelectedAnswer(index, option)}
             className='relative w-full rounded-lg flex items-center justify-center h-full cursor-pointer transition-colors duration-300'
